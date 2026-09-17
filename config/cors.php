9f1af07e20a9441f7c85f0b52911658cfd26c8d7
hakaiou <hakaiou20067@gmail.com>
2026-09-17 10:06:56 +0900

    configure production CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000',
+    'https://unicycle-app-react-production.up.railway.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
