072c2db5c12a23e5e0466bcb88761060934ea47c
hakaiou <hakaiou20067@gmail.com>
2026-09-18 16:50:50 +0900

    config update

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => ['http://localhost:3000',
-    'https://unicycle-app-react-production.up.railway.app',
+    'https://app.unicircle-jp.com',
     ],
 
     'allowed_origins_patterns' => [],
